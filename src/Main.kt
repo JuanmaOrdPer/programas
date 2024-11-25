5f426882fe6620670764712b22b31399b99a59e6
JuanmaOrdPer <juanma.ordas@gmail.com>
2024-11-25 18:51:48 +0100

    programa2

--- src/Main.kt
+++ src/Main.kt
@@ -2,14 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     println("Introduzca una palabra")
-    var palabra1 = readln()
+    val palabra1 = readln()
     println("Introduce otra palabra")
-    var palabra2 = readln()
+    val palabra2 = readln()
     println("Las palabras $palabra1 y $palabra2 con un anagrama? = " + anagrama(palabra1,palabra2))
 
 
 }
 fun anagrama( palabra1:String, palabra2: String): Boolean{
-    var reves = palabra1.reversed() //esta funcion la saque de https://www.develou.com/funcion-reversed-en-kotlin/
+    val reves = palabra1.reversed() //esta funcion la saque de https://www.develou.com/funcion-reversed-en-kotlin/
     return  (reves==palabra2)
 }
