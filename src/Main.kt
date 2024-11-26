3f26881f637f5a5b7debe5c95458992728f0a661
JuanmaOrdPer <juanma.ordas@gmail.com>
2024-11-26 19:50:56 +0100

    programa4

--- src/Main.kt
+++ src/Main.kt
@@ -1,40 +1,25 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-
-   println("¿Está balanceada? " + balanceada("{a + b [c] * (2x2)}}}}"))
-    println("¿Está balanceada? " + balanceada("{ [ a * ( c + d ) ] - 5 }"))
-    println("¿Está balanceada? " + balanceada("{ a * ( c + d ) ] - 5 }"))
-    println("¿Está balanceada? " + balanceada("{a^4 + (((ax4)}"))
-    println("¿Está balanceada? " + balanceada("{ ] a * ( c + d ) + ( 2 - 3 )[ - 5 }"))
-    println("¿Está balanceada? " + balanceada("{{{{{{(}}}}}}"))
-    println("¿Está balanceada? " + balanceada("(a"))
-
-
+val atleta1 = arrayOf("correr", "saltar", "correr", "saltar", "correr")
+val pista = "_|_|_"
+    competicion(atleta1, pista)
 
 }
 
-fun balanceada(str: String):Boolean {
-    val simbolos = mapOf("{" to "}" ,"[" to "]","(" to ")")
-    val lista = mutableListOf<String>()
-
-
-    str.forEach {
-        val simbolo = it.toString()
-        val apertura = simbolos.containsKey(simbolo)
-
-        if (apertura) {
-            lista.add(simbolo)
-        } else if (simbolos.containsValue(simbolo)) {
-            if (lista.isEmpty()) {
-                return false
-            }
-            val ultimoSimbolo = lista.removeAt(lista.size - 1)
-            if (simbolos[ultimoSimbolo] != simbolo) {
-                return false
-            }
-        }
+fun competicion(atleta: Array<String>, pista: String){
+    val mapa = mapOf("correr" to "_", "pista" to "|")
+    var carrera = mutableListOf<String>()
+    for (suelo in  pista) {
+        carrera.add(suelo.toString())
     }
-    return lista.isEmpty()
+
+
+
 }
+
+
+
+
+
 
